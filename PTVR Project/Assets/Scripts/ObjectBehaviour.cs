c239532aa2d1a7fd6450c3288ec6e4e24227ece8
Jarryd Worland <101096903@student.swin.edu.au>
2021-08-10 10:32:18 +1000

    - Inital Sandbox Development (DA movement to point)

---
+++ PTVR Project/Assets/Scripts/ObjectBehaviour.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectBehaviour : MonoBehaviour
+{
+    private const int SPEED = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void currentDestination()
+    {
+
+    }
+
+}
