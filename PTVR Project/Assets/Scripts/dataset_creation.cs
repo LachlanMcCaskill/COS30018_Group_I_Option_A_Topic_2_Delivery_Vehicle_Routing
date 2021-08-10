44e84b4982c0f48880470f5dc602234e8665d363
Lachlan McCaskill <101627963@student.swin.edu.au>
2021-08-10 10:26:33 +1000

    CHANGE: Renamed script classes so as to not cause unity errors

--- PTVR Project/Assets/Scripts/dataset_creation.cs
+++ PTVR Project/Assets/Scripts/dataset_creation.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class dataset_creation : MonoBehaviour
 {
     public long[,] DistanceMatrix = 
     {
