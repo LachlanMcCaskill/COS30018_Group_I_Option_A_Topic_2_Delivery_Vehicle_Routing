3a85057e525d19bd95760f85f4928ff8cf0c73d2
Jarryd Worland <101096903@student.swin.edu.au>
2021-08-24 10:16:43 +1000

    More Work added to autonomus movement for a gameobject (Not Currently functional)

--- PTVR Project/Assets/Scripts/ObjectBehaviour.cs
+++ PTVR Project/Assets/Scripts/ObjectBehaviour.cs
@@ -5,16 +5,41 @@ using UnityEngine;
 public class ObjectBehaviour : MonoBehaviour
 {
     private const int SPEED = 10;
+
     // Start is called before the first frame update
     void Start()
     {
-        Rigidbody rb = GetComponent<Rigidbody>();
+       
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector3 currentStop = new Vector3(100.0f, 100.0f, 0.0f);
+        GameObject stop = GameObject.Find("Point A");
+
+        GameObject bus = GameObject.Find("MovingCube");
+        Vector3 currentPosition = bus.transform.position;
+
+
+        if (bus.transform.position.x < currentStop.x)
+        {
+            transform.position = transform.position + new Vector3(currentPosition.x * SPEED * Time.deltaTime, 0, currentPosition.z);
+        }
+        else if (bus.transform.position.x > currentStop.x)
+        {
+            transform.position = transform.position + new Vector3(currentPosition.x * SPEED * Time.deltaTime, 0, currentPosition.z);
+        }
         
+        if (bus.transform.position.z < currentStop.z)
+        {
+            transform.position = transform.position + new Vector3(currentPosition.x, 0, currentPosition.z * SPEED * Time.deltaTime);
+        }
+        else if (bus.transform.position.z > currentStop.z)
+        {
+            transform.position = transform.position + new Vector3(currentPosition.x, 0, currentPosition.z * SPEED * Time.deltaTime);
+        }
+
     }
 
     private void currentDestination()
