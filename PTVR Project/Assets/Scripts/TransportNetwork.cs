8119c3aa6e4b394f2f2a4fdda152e0a08fa19ba9
Lachlan McCaskill <101627963@student.swin.edu.au>
2021-08-27 10:13:37 +1000

    NEW: Added transport network base object

---
+++ PTVR Project/Assets/Scripts/TransportNetwork.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransportNetwork : MonoBehaviour
+{
+    private GameObject start;
+    private List<GameObject> destinations;
+}
