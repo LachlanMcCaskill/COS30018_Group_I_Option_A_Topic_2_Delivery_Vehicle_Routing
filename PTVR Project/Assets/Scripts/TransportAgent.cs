dee95359e64df68652fd686987f165b0e302dcf7
Lachlan McCaskill <101627963@student.swin.edu.au>
2021-08-27 10:35:36 +1000

    NEW: Started work on transport agent class. Still in progress

---
+++ PTVR Project/Assets/Scripts/TransportAgent.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransportAgent : MonoBehaviour
+{
+    private List<Passenger> _passengers;
+    private int _capacity;
+    private Route currentRoute;
+
+    private void TravelToNextDestination()
+    {
+
+    }
+
+    private void AddPassenger(Passenger p)
+    {
+        _passengers.Add(p);
+    }
+
+    private void ArriveAtStop()
+    {
+
+    }
+
+    private void AssignRoute(Route r)
+    {
+
+    }
+
+    private void SendCapacity()
+    {
+
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
