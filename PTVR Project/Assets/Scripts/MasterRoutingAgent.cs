26349b38f5822d69ac79227c90a762beb7a00145
Lachlan McCaskill <101627963@student.swin.edu.au>
2021-08-28 17:31:59 +1000

    NEW: Added the base file for the master routing agent

---
+++ PTVR Project/Assets/Scripts/MasterRoutingAgent.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MasterRoutingAgent : MonoBehaviour
+{
+    private RouteSolver _routeSolver;
+    private TransportNetwork _transportNetwork;
+    private List<TransportAgent> _transportAgent;
+
+    private void SendRoutes()
+    {
+
+    }
+
+    private void CreateRoutes()
+    {
+
+    }
+
+    private float CalculateDistance(Vector3 point1, Vector3 point2)
+    {
+        float result = new float();
+        return result;
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
