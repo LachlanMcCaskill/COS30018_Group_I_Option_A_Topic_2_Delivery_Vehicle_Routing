0f1c2cfb0db9d8044ed23388e8b51d6270904abd
Lachlan McCaskill <101627963@student.swin.edu.au>
2021-08-31 09:07:43 +1000

    CHANGE: Added passenger constructor

--- PTVR Project/Assets/Scripts/Passenger.cs
+++ PTVR Project/Assets/Scripts/Passenger.cs
@@ -5,4 +5,9 @@ using UnityEngine;
 public class Passenger : MonoBehaviour
 {
     private Stop _intendedDestination;
+
+    Passenger(Stop destinationToSet)
+    {
+        _intendedDestination = destinationToSet;
+    }
 }
