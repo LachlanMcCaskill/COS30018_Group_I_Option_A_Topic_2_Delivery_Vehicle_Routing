753a6dc106e8e771349c0ceaff4eacf15ffd68b2
Jarryd Worland <101096903@student.swin.edu.au>
2021-09-07 10:09:16 +1000

    Added Start GUI and Exit Code

---
+++ PTVR Project/Assets/Scripts/MenuHandler.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class MenuHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void QuitApp()
+    {
+
+        if (EditorApplication.isPlaying)
+        {
+            EditorApplication.isPlaying = false;
+        }
+        else
+        {
+            Application.Quit();
+        }
+    }
+}
