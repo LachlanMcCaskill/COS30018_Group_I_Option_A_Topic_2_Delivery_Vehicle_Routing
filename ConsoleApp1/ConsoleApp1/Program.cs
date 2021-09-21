b7a60ead8edf275c3446d27b87da616b78380341
alistairtruman <1776614@student.swin.edu.au>
2021-09-21 10:28:57 +1000

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,112 +7,23 @@ using System.Numerics;
 
 namespace GeneticAlgorithmMTSP
 {
+
     class Program
     {
-        static void Main(string[] args)
-        {
-            Vector2[] points = new Vector2[12];
-            points[0] = new Vector2(0, 0);
-            points[1] = new Vector2(-1, -5);
-            points[2] = new Vector2(-5, -8);
-            points[3] = new Vector2(-3, 6);
-            points[4] = new Vector2(-7, 3);
-            points[5] = new Vector2(-3, 6);
-            points[6] = new Vector2(-9, 9);
-            points[7] = new Vector2(1, 6);
-            points[8] = new Vector2(5, 5);
-            points[9] = new Vector2(8, 6);
-            points[10] = new Vector2(4, -5);
-            points[11] = new Vector2(8, -3);
+        const int selectionSize = 20;
 
-            //
-            Route[] routes = new Route[2];
-
-            Random r = new Random();
-
-            //  Initial 10 routes
-            for (int i = 0; i < routes.Length; i++)
-            {
-                routes[i] = new Route(points, new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 }, i.ToString(), r);
-            }
-
-            //console.WriteLine("Route Distances: ");
-            int currentGeneration = 0;
-            int maxGeneration = 100;
-
-
-            float currentShortest = ShortestRoute(routes).TotalDistance;
-            float currentAverage = AverageDistance(routes);
-
-            Console.WriteLine("Generation " + currentGeneration + " Population: " + routes.Length + "\nAverage Distance: " + currentAverage
-                + "\nShortest Route: " + currentShortest);
-
-            while (currentGeneration < maxGeneration)
-            {
-                float oldShortest = currentShortest;
-                float oldAverage = currentAverage;
-
-                currentShortest = ShortestRoute(routes).TotalDistance;
-                currentAverage = AverageDistance(routes);
-
-                currentGeneration++;
-                RunNextGeneration(routes, r);
-
-                Console.Write("\nGeneration " + currentGeneration + " Population: " + routes.Length + "\nAverage Distance: " + currentAverage);
-                if (oldAverage < currentAverage)
-                {
-                    Console.WriteLine(" [" + (oldAverage - currentAverage) + "] slower");
-                }
-                else
-                {
-                    Console.WriteLine(" [" + (oldAverage - currentAverage) + "] faster");
-                }
-
-                Console.Write("Shortest Route: " + currentShortest);
-
-                if (oldShortest < currentShortest)
-                {
-                    Console.WriteLine(" [" + (oldShortest - currentShortest) + "] slower");
-                }
-                else
-                {
-                    Console.WriteLine(" [" + (oldShortest - currentShortest) + "] faster");
-                }
-            }
-        }
-        static void RunNextGeneration(Route[] routes, Random r)
-        {
-            Route[] temp = CopyRoutes(routes);
-
-            GetNextGeneration(routes, temp, r);
-
-            for (int i = 0; i < temp.Length; i++)
-            {
-                temp[i].name = null;
-                temp[i].order = null;
-                temp[i].points = null;
-                temp[i] = null;
-            }
-            temp = null;
-        }
-        static Route[] CopyRoutes(Route[] source)
-        {
-            Route[] newRoutes = new Route[source.Length];
-            for (int i = 0; i < source.Length; i++)
-            {
-                newRoutes[i] = new Route(source[i].points, source[i].order, source[i].name);
-            }
-            return newRoutes;
-        }
+        //  this is the fitness test, these are the important parts (assuming the rest works)
         static Route[] GetParents(Route[] routes, Random r)
         {
-            int[][] selection = PopulationSelection(0, routes.Length, 20, r);
+            int size = Math.Min(selectionSize, routes.Length / 2);
+            int[][] selection = PopulationSelection(0, routes.Length, size, r);
 
             Route mother = GetFittestRouteFromRandomSelection(routes, selection[0]);
             Route father = GetFittestRouteFromRandomSelection(routes, selection[1]); ;
 
             return new Route[] { mother, father };
         }
+
         static int[][] PopulationSelection(int min, int max, int size, Random r)
         {
             int select;
@@ -133,6 +44,223 @@ namespace GeneticAlgorithmMTSP
             }
             return rarry;
         }
+        // End fitness test
+
+        static void Main(string[] args)
+        {
+            //  Setting Varaibles
+            int[] vars = GetVariables();    //  user can input variables or use default variables
+            int numberPoints = vars[0];
+            int numberPopulation = vars[1];
+            int maxGeneration = vars[2];
+
+            Random r = new Random();    // create this here because of how c# random works, pass this if you need a random variable
+            int[] baseOrder = CreateOrder(numberPoints);    //  create an array of ints 0 to maxpoints
+            Vector2[] points = GeneratePoints(numberPoints, r); /// generate random points
+            Route[] routes = new Route[numberPopulation];   //  create empty array of routes
+
+            int currentGeneration = 0;  // generationloop compares the current and max generations till max is reached
+
+            //  Generate initial (randomised) routes
+            for (int i = 0; i < routes.Length; i++)
+            {
+                routes[i] = new Route(points, baseOrder, i.ToString(), r);
+            }
+
+            // Get First Generation Information
+            float currentShortest = ShortestRoute(routes).TotalDistance;    // shortest from CURRENTGENERATION not the currently fastest generated route
+            float currentAverage = AverageDistance(routes);                 // see above
+
+            PrintInitialGeneration(currentGeneration, numberPopulation, numberPoints, maxGeneration, currentAverage, currentShortest);
+
+            float firstRoute = currentShortest; // fastest route from initial random generation (if you can't beat this then the optimisation is not so good)
+            float fastestRoute = firstRoute;         // best route so far
+            int fastestGeneration = 0;      // generation of best route
+            float[] nGenerationAverages = new float[maxGeneration];   // where n is some division of max generations (ie average of each 100 generations)
+
+            // Generation Loop
+            while (currentGeneration < maxGeneration)
+            {
+                float oldShortest = currentShortest;
+                float oldAverage = currentAverage;
+
+                currentGeneration++;
+                RunNextGeneration(routes, r);
+
+                currentShortest = ShortestRoute(routes).TotalDistance;
+                currentAverage = AverageDistance(routes);
+
+                PrintSubsequentGeneration(currentGeneration, numberPopulation, numberPoints, maxGeneration, currentAverage, currentShortest, oldAverage, oldShortest);
+
+                if (currentShortest < fastestRoute)
+                {
+                    fastestRoute = currentShortest;
+                    fastestGeneration = currentGeneration;
+                }
+            }
+
+            PrintFinalResults(firstRoute, fastestRoute, fastestGeneration, nGenerationAverages, currentShortest);
+
+            int.Parse(Console.ReadLine());
+        }
+
+        static void PrintFinalResults(float firstRoute, float fastestRoute, int fastestGeneration, float[] generationAverages, float finalRoute)
+        {
+            Console.WriteLine("Evolution complete.");
+            Console.WriteLine("The best route in the initial generation was " + firstRoute);
+            Console.WriteLine("The best route in the final generation was " + finalRoute);
+
+            if (finalRoute < firstRoute)
+            {
+                Console.WriteLine("Which was " + (firstRoute - finalRoute) + " faster than the initial fastest route ");
+            }
+            else
+            {
+                Console.WriteLine("Which was " + (finalRoute - firstRoute) + " slower than the initial fastest route :(");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("The fastest route was " + fastestRoute + " in generation " + fastestGeneration);
+
+            if (fastestGeneration == 0)
+            {
+                Console.WriteLine("Woops the random generation beat your optimisation algorithm...");
+            }
+            else
+            {
+                Console.WriteLine("Which was " + (firstRoute - fastestRoute) + " faster than the initial fastest route ");
+            }
+        }
+
+        static int[] CreateOrder(int count)
+        {
+            // just generates an array of sequential ints (this is basically the chromosome)
+            int[] numberPoints = new int[count];
+            for (int i = 0; i < count; i++)
+            {
+                numberPoints[i] = i;
+            }
+            return numberPoints;
+        }
+
+        static Vector2[] GeneratePoints(int count, Random r)
+        {
+            Vector2[] points = new Vector2[count];
+            for (int i = 0; i < count; i++)
+            {
+                float x = (float)r.NextDouble() * 10;
+                float y = (float)r.NextDouble() * 10;
+                points[i] = new Vector2(x, y);
+            }
+            return points;
+        }
+
+        static public int[] GetVariables()
+        {
+            //  points, population, generation 
+            int input = 0;
+            int numberPoints = 100;
+            int numberGenerations = 1000;
+            int numberPopulation = 1000;
+            int[] vars = new int[] { numberPoints, numberPopulation, numberGenerations };
+            Console.WriteLine("Enter number of points: (Enter 0 for defaults)");
+            input = int.Parse(Console.ReadLine());
+            if (input == 0)
+            {
+                return vars;
+            }
+            else numberPoints = input;
+
+            input = 0;  // reset for validation
+
+            while (input == 0)
+            {
+                Console.WriteLine("Number of generations:");
+                input = int.Parse(Console.ReadLine());
+            }
+            numberGenerations = input;
+            input = 0;  // reset for validation
+
+            while (input == 0)
+            {
+                Console.WriteLine("Population number:");
+                input = int.Parse(Console.ReadLine());
+            }
+            numberPopulation = input;
+            input = 0;  // reset for validation
+
+            vars = new int[] { numberPoints, numberPopulation, numberGenerations };
+
+            return vars;
+        }
+
+        static void GenerationLoop()
+        {
+
+        }
+
+        static void PrintSubsequentGeneration(int currentGeneration, int numberPopulation, int numberPoints, int numberGenerations, float currentAverage, float currentShortest, float oldAverage, float oldShortest)
+        {
+            Console.WriteLine("Generation " + currentGeneration + " (Population: " + numberPopulation + ", Points " + numberPoints + ", Generations: " + numberGenerations + ")");
+
+            Console.Write("Average Route: " + currentAverage);
+            if (oldAverage < currentAverage)
+            {
+                Console.WriteLine(" [" + (oldAverage - currentAverage) + "] slower");
+            }
+            else
+            {
+                Console.WriteLine(" [" + (oldAverage - currentAverage) + "] faster");
+            }
+
+            Console.Write("Shortest Route: " + currentShortest);
+            if (oldShortest < currentShortest)
+            {
+                Console.WriteLine(" [" + (oldShortest - currentShortest) + "] slower");
+            }
+            else
+            {
+                Console.WriteLine(" [" + (oldShortest - currentShortest) + "] faster");
+            }
+
+            Console.WriteLine();
+            //Console.WriteLine("Generation " + currentGeneration + " (Population: " + numberPopulation + ", Points " + numberPoints + ", Generations: + " + numberGenerations + ")\nAverage Distance: " + currentAverage
+            //      + "\nShortest Route: " + currentShortest);
+        }
+
+        static void PrintInitialGeneration(int currentGeneration, int numberPopulation, int numberPoints, int numberGenerations, float currentAverage, float currentShortest)
+        {
+            Console.WriteLine("Generation " + currentGeneration + " (Population: " + numberPopulation + ", Points " + numberPoints + ", Generations: " + numberGenerations + ")\nAverage Distance: " + currentAverage
+                + "\nShortest Route: " + currentShortest);
+            Console.WriteLine();
+        }
+
+        static void RunNextGeneration(Route[] routes, Random r)
+        {
+            Route[] temp = CopyRoutes(routes);
+
+            GetNextGeneration(routes, temp, r);
+
+            for (int i = 0; i < temp.Length; i++)
+            {
+                temp[i].name = null;
+                temp[i].order = null;
+                temp[i].points = null;
+                temp[i] = null;
+            }
+            temp = null;
+        }
+
+        static Route[] CopyRoutes(Route[] source)
+        {
+            Route[] newRoutes = new Route[source.Length];
+            for (int i = 0; i < source.Length; i++)
+            {
+                newRoutes[i] = new Route(source[i].points, source[i].order, source[i].name);
+            }
+            return newRoutes;
+        }
+
         static void GetNextGeneration(Route[] routes, Route[] temp, Random r)
         {
             for (int i = 0; i < routes.Length; i++)
@@ -150,6 +278,7 @@ namespace GeneticAlgorithmMTSP
             { distance += r.TotalDistance; }
             return distance / routes.Length;
         }
+
         static Route GetFittestRouteFromRandomSelection(Route[] routes, int[] selection)
         {
             Route fittest = routes[selection[0]];
@@ -162,6 +291,7 @@ namespace GeneticAlgorithmMTSP
             }
             return fittest;
         }
+
         static Route ShortestRoute(Route[] routes)
         {
             Route shortest = routes[0];
@@ -198,15 +328,16 @@ namespace GeneticAlgorithmMTSP
         }
         public void PrintGenes()
         {
+            Console.WriteLine(name);
             foreach (int g in order)
             {
-                //console.Write(g + " ");
+                Console.Write(g + " ");
                 if (g < 10)
                 {
-                    //console.Write(" "); // want spaces between genes the same
+                    Console.Write(" "); // want spaces between genes the same
                 }
             }
-            //console.WriteLine();
+            Console.WriteLine();
         }
         public float TotalDistance
         {
@@ -279,7 +410,7 @@ namespace GeneticAlgorithmMTSP
             // this is the constructor for a child
             // ordered crossover https://towardsdatascience.com/evolution-of-a-salesman-a-complete-genetic-algorithm-tutorial-for-python-6fe5d2b3ca35
 
-            name = mother.name + father.name;
+            //  name = (mother.name + father.name).Substring(0, ;
             //points = mother.points;
             order = new int[points.Length];
 
@@ -314,8 +445,8 @@ namespace GeneticAlgorithmMTSP
             }
 
             //console.Write("Mother: ");
-            mother.PrintGenes();
-            mother.PrintDistance();
+            //  mother.PrintGenes();
+            //  mother.PrintDistance();
             //console.Write("Father: ");
             for (int i = 0; i < order.Length; i++)
             {
@@ -333,16 +464,16 @@ namespace GeneticAlgorithmMTSP
                 }
             }
             //console.WriteLine();
-            father.PrintDistance();
+            //  father.PrintDistance();
             //  father.PrintGenes();
             //console.Write("Child:  ");
-            PrintGenes();
-            PrintDistance();
+            //  PrintGenes();
+            //  PrintDistance();
             CheckDuplicates();
             //console.Write("Child (After Mutation):  ");
             Mutate(r);
-            PrintGenes();
-            PrintDistance();
+            //  PrintGenes();
+            //  PrintDistance();
         }
 
         void CheckDuplicates()
