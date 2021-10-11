ddf667f4f203adce51b940068f9ed57098b09817
Lachlan McCaskill <101627963@student.swin.edu.au>
2021-10-12 09:52:29 +1100

    NEW: Added a script for the main menu scene

---
+++ PTVR Project/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RouteSolver;
+using UnityEditor;
+
+public class MenuScript : MonoBehaviour
+{
+    [SerializeField]private string _targetsceneName;
+
+    public void loadGameSceneGreedy1()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(_targetsceneName);
+        PlayerPrefs.SetString("RoutingStrategy", "Greedy1");
+    }
+
+    public void loadGameSceneGreedy2()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(_targetsceneName);
+        PlayerPrefs.SetString("RoutingStrategy", "Greedy2");
+    }
+
+    public void loadGameSceneGeneticAlgorithm()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(_targetsceneName);
+        PlayerPrefs.SetString("RoutingStrategy", "GA");
+    }
+
+    public void loadGameSceneKMeans1()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(_targetsceneName);
+        PlayerPrefs.SetString("RoutingStrategy", "KMeans1");
+    }
+
+    public void loadGameSceneKMeans2()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(_targetsceneName);
+        PlayerPrefs.SetString("RoutingStrategy", "KMeans2");
+    }
+
+    public void QuitApp()
+    {
+        if (EditorApplication.isPlaying)
+        {
+            EditorApplication.isPlaying = false;
+        }
+        else
+        {
+            Application.Quit();
+        }
+        Debug.Log("Debug: Exit button pressed.");
+    }
+}
